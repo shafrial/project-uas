3d3949d8b5340934d6a012493b5be75a07859566
shafrial <shafrialazis2005@gmail.com>
2023-11-18 13:59:41 +0700

    Membuat halaman jawa tengah

---
+++ script2.js
@@ -0,0 +1,24 @@
+const logo = document.querySelector('.logo img');
+const kotajatim = document.querySelectorAll('.kotajatim li');
+
+kotajatim.forEach(item => {
+    item.addEventListener('mouseover', function() {
+        const city = this.getAttribute('data-city');
+        // Ganti src gambar sesuai dengan kota yang di-hover
+        if (city === 'Surabaya') {
+            logo.src = 'surabaya.jpg';
+        } else if (city === 'Malang') {
+            logo.src = 'malang.jpg';
+        } else if (city === 'Kediri') {
+            logo.src = 'kediri.jpg';
+        } 
+        // Tampilkan gambar
+        logo.style.display = 'block';
+    });
+
+    item.addEventListener('mouseout', function() {
+        // Sembunyikan gambar ketika kursor tidak lagi di atas daftar kota
+        logo.src = '1698753199545.png';
+        logo.style.display = 'block';
+    });
+});
