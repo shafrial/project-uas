31e0c9c8e4bd2f3b7d45068feb1270ac0a5cf706
shafrial <shafrialazis2005@gmail.com>
2023-11-19 14:47:58 +0700

    Menambah asset dan menambah overflow pada list

--- script.js
+++ script.js
@@ -5,13 +5,27 @@ kotajatim.forEach(item => {
     item.addEventListener('mouseover', function() {
         const city = this.getAttribute('data-city');
         // Ganti src gambar sesuai dengan kota yang di-hover
-        if (city === 'Surabaya') {
-            logo.src = 'assets/surabaya.jpg';
+        if (city === 'Banyuwangi') {
+            logo.src = 'assets/banyuwangi.jpg';
         } else if (city === 'Malang') {
             logo.src = 'assets/malang.jpg';
         } else if (city === 'Kediri') {
             logo.src = 'assets/kediri.jpg';
-        } 
+        } else if (city === 'Gresik') {
+            logo.src = 'assets/gresik.png';
+        } else if (city === 'Jember') {
+            logo.src = 'assets/jember.png';
+        } else if (city === 'Pasuruan') {
+            logo.src = 'assets/pasuruan.jpg';
+        } else if (city === 'Ponorogo') {
+            logo.src = 'assets/ponorogo.jpg';
+        } else if (city === 'Blitar') {
+            logo.src = 'assets/blitar.jpg';
+        } else if (city === 'Sidoarjo') {
+            logo.src = 'assets/sidoarjo.jpg';
+        } else if (city === 'Surabaya') {
+            logo.src = 'assets/surabaya.jpg';
+        }
         // Tampilkan gambar
         logo.style.display = 'block';
     });
